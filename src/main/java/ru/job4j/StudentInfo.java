87a58a841207cf83742773da27115014316adf53
nikitakocnev <kochnev.n@octopod.ru>
2022-11-28 22:17:05 +0500

    6. Checkstyle [#4123]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Kochnev");
+        System.out.println("14.05.1996");
+    }
+}
