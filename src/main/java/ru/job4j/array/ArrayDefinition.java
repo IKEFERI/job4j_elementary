a1115245714e9175185d15fe1423eef79484fb8d
Nikita_Kochnev <remkefer@gmail.com>
2025-03-30 10:59:35 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Andrei Bosyi";
+        names[1] = "Nikita Kochnev";
+        names[2] = "Andrei Plahotin";
+        names[3] = "Dmitry Stukov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
