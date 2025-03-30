e8cf6bef330f7f10686b2e021177dd2deebf13c2
Nikita_Kochnev <remkefer@gmail.com>
2025-03-30 10:31:01 +0500

    6.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println('H');
+        for (int i = 0; i < 1; i++) {
+            System.out.println(i);
+        }
+    }
+}
